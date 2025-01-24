513a97b635f3405beba638c0b9d7e2e0e3cc59e3
daliso banda <bandadaliso255@gmail.com>
2025-01-25 00:13:14 +0200

    Add motor control definitions and update command handling structure

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,22 @@
 BluetoothSerial SerialBT;
 String deviceName = "RobotController";
 
+// DEFINE MOTOR PINS 
+#define  LEFT_MOTOR_FOREWARD 1
+#define  RIGHT_MOTOR_FOREWARD 5
+#define  LEFT_MOTOR_BACKWARD 16
+#define  RIGHT_MOTOR_BACKWARD 17
+
 void setup() {
   Serial.begin(9600);
   SerialBT.begin(deviceName);
   Serial.println("Bluetooth Device Ready");
+
+  // SET PIN MODE 
+  pinMode(RIGHT_MOTOR_FOREWARD, OUTPUT);
+   pinMode(LEFT_MOTOR_BACKWARD, OUTPUT);
+    pinMode(RIGHT_MOTOR_BACKWARD, OUTPUT);
+     pinMode(LEFT_MOTOR_FOREWARD, OUTPUT);
 }
 
 void loop() {
@@ -18,11 +30,45 @@ void loop() {
     
     // Optional: Direct command processing
     switch(command) {
-      case 'F': Serial.println("Forward Command"); break;
-      case 'B': Serial.println("Backward Command"); break;
-      case 'L': Serial.println("Left Command"); break;
-      case 'R': Serial.println("Right Command"); break;
-      case 'S': Serial.println("Stop Command"); break;
+      case 'F': 
+        // CONTROL MOTOR FORWARD
+        digitalWrite(RIGHT_MOTOR_FOREWARD, 1);
+        digitalWrite(LEFT_MOTOR_FOREWARD, 1);
+        digitalWrite(RIGHT_MOTOR_BACKWARD, 0);
+        digitalWrite(LEFT_MOTOR_BACKWARD, 0);
+        
+
+       break;
+      case 'B': 
+        // CONTROL MOTOR BACKWARD
+        digitalWrite(RIGHT_MOTOR_FOREWARD, 0);
+        digitalWrite(LEFT_MOTOR_FOREWARD, 0);
+         digitalWrite(LEFT_MOTOR_BACKWARD, 1);
+        digitalWrite(RIGHT_MOTOR_BACKWARD, 1);
+       break;
+      case 'L': 
+      // CONTROL MOTOR LEFT
+     digitalWrite(RIGHT_MOTOR_FOREWARD, 1);
+        digitalWrite(LEFT_MOTOR_FOREWARD, 0);
+         digitalWrite(LEFT_MOTOR_BACKWARD, 1);
+        digitalWrite(RIGHT_MOTOR_BACKWARD, 0);
+
+       break;
+      case 'R': 
+      //CONTROL MOTOR RIGHT 
+       digitalWrite(RIGHT_MOTOR_FOREWARD, 0);
+        digitalWrite(LEFT_MOTOR_FOREWARD, 1);
+         digitalWrite(LEFT_MOTOR_BACKWARD, 0);
+        digitalWrite(RIGHT_MOTOR_BACKWARD, 1);
+       break;
+      case 'O': 
+      //CONTROL MOTOR STOP
+       digitalWrite(RIGHT_MOTOR_FOREWARD, 0);
+        digitalWrite(LEFT_MOTOR_FOREWARD, 0);
+         digitalWrite(LEFT_MOTOR_BACKWARD, 0);
+        digitalWrite(RIGHT_MOTOR_BACKWARD, 0);
+       break;
+      default: Serial.println(command); break;
     }
   }
 }
